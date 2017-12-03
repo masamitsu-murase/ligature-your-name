157874f6b03c402f91e78001d0002ae0ce389d4c
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-12-03 15:45:08 +0900

    Add fontforge_job.

---
+++ app/jobs/fontforge_job.rb
@@ -0,0 +1,7 @@
+class FontforgeJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/fontforge_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FontforgeJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
